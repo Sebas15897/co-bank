ba0fefe518f92fb4d6d91b1c093ae130b9d3abff
Sebas15897 <sebastiancontreras15897@gmail.com>
2023-03-27 18:27:25 -0500

    feat: It is added - configuration for the environments - Angular material.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AuthPrivateGuard } from './core/guards/auth-private-guard/auth-private-guard.guard';
 import { AuthPublicGuard } from './core/guards/auth-public-guard/auth-public-guard.guard';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [AuthPublicGuard, AuthPrivateGuard],
   bootstrap: [AppComponent],
 })
