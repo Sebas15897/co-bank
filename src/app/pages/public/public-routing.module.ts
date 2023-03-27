2795950edaedf25bfddedcc01e221d1b15ad4c96
Sebas15897 <sebastiancontreras15897@gmail.com>
2023-03-27 18:15:52 -0500

    feat: Modular structure and routes for public and private sessions are added.

---
+++ src/app/pages/public/public-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthPublicGuard } from 'src/app/core/guards/auth-public-guard/auth-public-guard.guard';
+
+const routes: Routes = [
+  {
+    path: 'public',
+    children: [
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./login/login.module').then((m) => m.LoginModule),
+      },
+    ],
+    canActivate: [AuthPublicGuard],
+  },
+  {
+    path: '',
+    redirectTo: 'public/login',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PublicRoutingModule {}
