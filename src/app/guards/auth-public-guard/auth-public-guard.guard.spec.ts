e928b78436920dfbbc37f538e64036ed4d3582ab
Sebas15897 <sebastiancontreras15897@gmail.com>
2023-03-27 18:08:36 -0500

    feat: Guardians are created to manage a public module and a private module.

---
+++ src/app/guards/auth-public-guard/auth-public-guard.guard.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AuthPublicGuard } from './auth-public-guard.guard';
+
+describe('AuthPublicGuardGuard', () => {
+  let guard: AuthPublicGuard;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    guard = TestBed.inject(AuthPublicGuard);
+  });
+
+  it('should be created', () => {
+    expect(guard).toBeTruthy();
+  });
+});
---
+++ src/app/guards/auth-public-guard/auth-public-guard.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Store } from '@ngxs/store';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class AuthPublicGuard implements CanActivate {
+  constructor(private router: Router, private store: Store) {}
+
+  canActivate() {
+    return true;
+  }
+}
