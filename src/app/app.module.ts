dcd14d6962f1e03e3ff61675ab80ccef1ffdc5ed
Sebas15897 <sebastiancontreras15897@gmail.com>
2023-03-27 18:33:22 -0500

    feat: added router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppComponent } from './app.component';
 import { AuthPrivateGuard } from './core/guards/auth-private-guard/auth-private-guard.guard';
 import { AuthPublicGuard } from './core/guards/auth-public-guard/auth-public-guard.guard';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [AuthPublicGuard, AuthPrivateGuard],
   bootstrap: [AppComponent],
 })
