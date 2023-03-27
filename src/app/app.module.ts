79d6425a7623a7193f848acc435a3a9fd8a8a5cf
Sebas15897 <sebastiancontreras15897@gmail.com>
2023-03-27 18:43:06 -0500

    feat: finish project structire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,17 @@ import { AuthPrivateGuard } from './core/guards/auth-private-guard/auth-private-
 import { AuthPublicGuard } from './core/guards/auth-public-guard/auth-public-guard.guard';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app.routing.module';
+import { StateModule } from './core/state/state.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    StateModule,
+  ],
   providers: [AuthPublicGuard, AuthPrivateGuard],
   bootstrap: [AppComponent],
 })
-
 export class AppModule {}
